0923c481b549d0e4f8689ba74c74db4ae2d0019d
BabenkoMaksym <Maksym.Babenko.bbn@gmail.com>
2023-04-24 18:55:48 +0200

    Homework14 - done

--- src/main/java/ua/goit/java8/homeworkSpring/Note.java
+++ src/main/java/ua/goit/java8/homeworkSpring/Note.java
@@ -2,9 +2,14 @@ package ua.goit.java8.homeworkSpring;
 
 import lombok.Data;
 
-@Data
+import javax.persistence.*;
 
+@Data
+@Entity
+@Table(schema = "spring_hw")
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
--- src/main/java/ua/goit/java8/homeworkSpring/NoteService.java
+++ src/main/java/ua/goit/java8/homeworkSpring/NoteService.java
@@ -1,5 +1,7 @@
 package ua.goit.java8.homeworkSpring;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,41 +10,33 @@ import java.util.Map;
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class NoteService {
-    private final Map<Long, Note> notes = new HashMap<>();
     private final Random random = new Random();
 
+    private final NoteRepository repository;
+
     public List<Note> listAll() {
-        return new ArrayList<>(notes.values());
+        return repository.findAll();
     }
 
     public Note add(Note note) {
         long id = random.nextLong();
         note.setId(id);
-        notes.put(id, note);
+        repository.save(note);
         return note;
     }
 
     public void deleteById(long id) {
-        if (!notes.containsKey(id)) {
-            throw new IllegalArgumentException("Note not found for id: " + id);
-        }
-        notes.remove(id);
+       repository.deleteById(id);
     }
 
     public void update(Note note) {
-        if (!notes.containsKey(note.getId())) {
-            throw new IllegalArgumentException("Note not found for id: " + note.getId());
-        }
-        notes.put(note.getId(), note);
+        repository.save(note);
     }
 
     public Note getById(long id) {
-        Note note = notes.get(id);
-        if (note == null) {
-            throw new IllegalArgumentException("Note not found for id: " + id);
-        }
-        return note;
+        return repository.getReferenceById(id);
     }
 }
 
